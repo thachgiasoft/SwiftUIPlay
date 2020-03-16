05ea0228a8c7ce3640e8383a2e088472513283d8
吕丁阳 <1297486818@qq.com>
2020-03-16 17:29:08 +0800

    Add a chapter about the Gestures.

---
+++ SwiftUIPlay/Gestures.swift
@@ -0,0 +1,21 @@
+//
+//  Gestures.swift
+//  SwiftUIPlay
+//
+//  Created by 吕丁阳 on 2020/3/16.
+//  Copyright © 2020 吕丁阳. All rights reserved.
+//
+
+import SwiftUI
+
+struct Gestures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Gestures_Previews: PreviewProvider {
+    static var previews: some View {
+        Gestures()
+    }
+}
